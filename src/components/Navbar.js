62fbf41f6a54725a16648dbd39914274ecb28267
touhidulislamkhan <touhidikhan0@gmail.com>
2021-05-18 23:42:28 +0600

    adding router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="">Navbar</a>
+                    <a className="navbar-brand" href="">Admin-Dash</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
